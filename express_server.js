4fa0c1907f950a80d04cdaf242150d913a1f165d
Nikko B <ncbadoles@gmail.com>
2020-08-03 19:15:20 +0000

    Using ejs for the first time and added views folder with ejs file

--- express_server.js
+++ express_server.js
@@ -7,13 +7,39 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// We need to specify the template ejs using the set below. => ejs
+// create a views folder
+app.set('view engine', 'ejs');
+
+// add a new route handler for "/urls" and use res.render() to pass the URL data to our template.
+app.get('/urls', (req, res) =>{
+    let templateVars = { urls:urlDatabase };
+    res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
+
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
+
+
+// ADDED new route for experimental purposes//
+
+app.get("/set", (req, res) => {
+    const a = 1;
+    res.send(`a = ${a}`);
+});
+   
+app.get("/fetch", (req, res) => {
+    res.send(`a = ${a}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

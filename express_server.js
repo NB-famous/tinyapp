ce167d70093210e169130bbf538318e3d6ffd338
Nikko B <ncbadoles@gmail.com>
2020-08-03 20:26:34 +0000

    added boby-parser and included a function that generate 6 random string char

--- express_server.js
+++ express_server.js
@@ -1,3 +1,14 @@
+//Generate a Random ShortURL using a function 
+function generateRandomString() {
+
+    let randomString = Math.random().toString(30).substring(2, 8);
+
+    return randomString;
+
+}
+
+
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -6,6 +17,11 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+// Adding body parser parkage "yarn add body-parser"
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // We need to specify the template ejs using the set below. => ejs
 // create a views folder
@@ -17,9 +33,14 @@ app.get('/urls', (req, res) =>{
     res.render("urls_index", templateVars);
 });
 
+// Adding a GET Route to Show the Form
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
+
 // Adding a new route
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get("/urls/:id", (req, res) => {
     let templateVars = { shortURL: req.params.shortURL, longURL:"http://www.lighthouselabs.ca"};
     res.render("urls_show", templateVars);
   });
@@ -37,17 +58,26 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
+//Adding a POST Route to Receive the Form Submission
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 // ADDED new route for experimental purposes//
-
-app.get("/set", (req, res) => {
+/*
+ app.get("/set", (req, res) => {
     const a = 1;
     res.send(`a = ${a}`);
 });
    
 app.get("/fetch", (req, res) => {
     res.send(`a = ${a}`);
-});
+}); 
+
+*/
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
